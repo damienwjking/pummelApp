2850fcb32ec057367787cf22c276bc33b616eada
Damien King <damien@pummel.me>
2016-02-29 20:52:34 +1000

    New Register view controller

---
+++ pummel/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  pummel
+//
+//  Created by Damien King on 29/02/2016.
+//  Copyright © 2016 pummel. All rights reserved.
+//
+
+import UIKit
+
+
+class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
