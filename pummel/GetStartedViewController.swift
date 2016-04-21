4dc0b84f21ae5099622dcb8c7f4155c2088061c2
JackNguyen-zz <ng.th.thong@gmail.com>
2016-04-21 17:16:19 +1000

    autolayout for ip6plus

--- pummel/GetStartedViewController.swift
+++ pummel/GetStartedViewController.swift
@@ -16,6 +16,8 @@ class GetStartedViewController: UIViewController {
     @IBOutlet var imNewBT : UIButton!
     @IBOutlet var getStartedBT : UIButton!
     @IBOutlet var backgroundV : UIView!
+    @IBOutlet var imNewWidthCT: NSLayoutConstraint!
+    @IBOutlet var getStartWidthCT: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +38,8 @@ class GetStartedViewController: UIViewController {
         self.imNewBT.layer.borderColor = UIColor(red:1, green: 91.0/255.0, blue: 16.0/255.0, alpha: 1.0).CGColor
         self.imNewBT.layer.backgroundColor = UIColor(red:1, green: 91.0/255.0, blue: 16.0/255.0, alpha: 1.0).CGColor
         self.imNewBT.titleLabel?.font = UIFont(name: "Montserrat-Regular", size: 13)
+        
+        self.updateUI()
     }
     
     // Button Action
@@ -54,6 +58,20 @@ class GetStartedViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateUI() {
+        let SCREEN_WIDTH         = UIScreen.mainScreen().bounds.size.width
+        let SCREEN_HEIGHT        = UIScreen.mainScreen().bounds.size.height
+        let SCREEN_MAX_LENGTH    = max(SCREEN_WIDTH, SCREEN_HEIGHT)
+        if (UIDevice.currentDevice().userInterfaceIdiom == .Phone && SCREEN_MAX_LENGTH == 568.0) {
+            self.imNewWidthCT.constant = 137.0
+            self.getStartWidthCT.constant = 138.0
+        }
+        if (UIDevice.currentDevice().userInterfaceIdiom == .Phone && SCREEN_MAX_LENGTH == 736.0) {
+            self.imNewWidthCT.constant = 180.0
+            self.getStartWidthCT.constant = 180.0
+        }
     }
     
     
