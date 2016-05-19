9a51433ec028c616c0c8111895d494d04c634ee2
JackNguyen-zz <ng.th.thong@gmail.com>
2016-05-19 10:15:29 +1000

    Missing file

---
+++ pummel/Sources/FSAlbumView.swift
@@ -0,0 +1,364 @@
+//
+//  FSAlbumView.swift
+//  Fusuma
+//
+//  Created by Thong Nguyen on 2015/11/14.
+//  Copyright © 2015年 Thong Nguyen. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+@objc public protocol FSAlbumViewDelegate: class {
+    
+    func albumViewCameraRollUnauthorized()
+}
+
+final class FSAlbumView: UIView, UICollectionViewDataSource, UICollectionViewDelegate, PHPhotoLibraryChangeObserver, UIGestureRecognizerDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var collectionViewConstraintHeight: NSLayoutConstraint!
+    
+    weak var delegate: FSAlbumViewDelegate? = nil
+    var imageSelected : UIImage!
+    var images: PHFetchResult!
+    var imageManager: PHCachingImageManager?
+    var previousPreheatRect: CGRect = CGRectZero
+    let cellSize = CGSize(width: 100, height: 100)
+    
+    // Variables for calculating the position
+    enum Direction {
+        case Scroll
+        case Stop
+        case Up
+        case Down
+    }
+    let imageCropViewOriginalConstraintTop: CGFloat = 50
+    let imageCropViewMinimalVisibleHeight: CGFloat  = 100
+    var dragDirection = Direction.Up
+    var imaginaryCollectionViewOffsetStartPosY: CGFloat = 0.0
+    
+    var cropBottomY: CGFloat  = 0.0
+    var dragStartPos: CGPoint = CGPointZero
+    let dragDiff: CGFloat     = 20.0
+    
+    static func instance() -> FSAlbumView {
+        
+        return UINib(nibName: "FSAlbumView", bundle: NSBundle(forClass: self.classForCoder())).instantiateWithOwner(self, options: nil)[0] as! FSAlbumView
+    }
+    
+    func initialize() {
+        
+        if images != nil {
+            
+            return
+        }
+		
+		self.hidden = false
+        
+     
+        dragDirection = Direction.Up
+        
+        
+        collectionView.registerNib(UINib(nibName: "FSAlbumViewCell", bundle: NSBundle(forClass: self.classForCoder)), forCellWithReuseIdentifier: "FSAlbumViewCell")
+		collectionView.backgroundColor = fusumaBackgroundColor
+		
+        // Never load photos Unless the user allows to access to photo album
+        checkPhotoAuth()
+        
+        // Sorting condition
+        let options = PHFetchOptions()
+        options.sortDescriptors = [
+            NSSortDescriptor(key: "creationDate", ascending: false)
+        ]
+        
+        images = PHAsset.fetchAssetsWithMediaType(.Image, options: options)
+        
+        if images.count > 0 {
+            
+            changeImage(images[0] as! PHAsset)
+            collectionView.reloadData()
+            collectionView.selectItemAtIndexPath(NSIndexPath(forRow: 0, inSection: 0), animated: false, scrollPosition: UICollectionViewScrollPosition.None)
+        }
+        
+        PHPhotoLibrary.sharedPhotoLibrary().registerChangeObserver(self)
+        
+    }
+    
+    deinit {
+        
+        if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.Authorized {
+            
+            PHPhotoLibrary.sharedPhotoLibrary().unregisterChangeObserver(self)
+        }
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        
+        return true
+    }
+    
+    
+    // MARK: - UICollectionViewDelegate Protocol
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("FSAlbumViewCell", forIndexPath: indexPath) as! FSAlbumViewCell
+        
+        let currentTag = cell.tag + 1
+        cell.tag = currentTag
+        
+        let asset = self.images[indexPath.item] as! PHAsset
+        self.imageManager?.requestImageForAsset(asset,
+            targetSize: cellSize,
+            contentMode: .AspectFill,
+            options: nil) {
+                result, info in
+                
+                if cell.tag == currentTag {
+                    cell.image = result
+                }
+                
+        }
+        
+        return cell
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return images == nil ? 0 : images.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        let width = (collectionView.frame.width - 3) / 4
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let asset = images[indexPath.row] as! PHAsset
+        
+        let options = PHImageRequestOptions()
+        options.networkAccessAllowed = true
+        
+        self.imageManager?.requestImageForAsset(asset,
+            targetSize: CGSize(width: asset.pixelWidth, height: asset.pixelHeight),
+            contentMode: .AspectFill,
+            options: options) {
+                result, info in
+                self.imageSelected = result
+        }
+        
+        dragDirection = Direction.Up
+        collectionView.scrollToItemAtIndexPath(indexPath, atScrollPosition: .Top, animated: true)
+    }
+    
+    
+    // MARK: - ScrollViewDelegate
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+        if scrollView == collectionView {
+            self.updateCachedAssets()
+        }
+    }
+    
+    
+    //MARK: - PHPhotoLibraryChangeObserver
+    func photoLibraryDidChange(changeInstance: PHChange) {
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            
+            let collectionChanges = changeInstance.changeDetailsForFetchResult(self.images)
+            if collectionChanges != nil {
+                
+                self.images = collectionChanges!.fetchResultAfterChanges
+                
+                let collectionView = self.collectionView!
+                
+                if !collectionChanges!.hasIncrementalChanges || collectionChanges!.hasMoves {
+                    
+                    collectionView.reloadData()
+                    
+                } else {
+                    
+                    collectionView.performBatchUpdates({
+                        let removedIndexes = collectionChanges!.removedIndexes
+                        if (removedIndexes?.count ?? 0) != 0 {
+                            collectionView.deleteItemsAtIndexPaths(removedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
+                        }
+                        let insertedIndexes = collectionChanges!.insertedIndexes
+                        if (insertedIndexes?.count ?? 0) != 0 {
+                            collectionView.insertItemsAtIndexPaths(insertedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
+                        }
+                        let changedIndexes = collectionChanges!.changedIndexes
+                        if (changedIndexes?.count ?? 0) != 0 {
+                            collectionView.reloadItemsAtIndexPaths(changedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
+                        }
+                        }, completion: nil)
+                }
+                
+                self.resetCachedAssets()
+            }
+        }
+    }
+}
+
+internal extension UICollectionView {
+    
+    func aapl_indexPathsForElementsInRect(rect: CGRect) -> [NSIndexPath] {
+        let allLayoutAttributes = self.collectionViewLayout.layoutAttributesForElementsInRect(rect)
+        if (allLayoutAttributes?.count ?? 0) == 0 {return []}
+        var indexPaths: [NSIndexPath] = []
+        indexPaths.reserveCapacity(allLayoutAttributes!.count)
+        for layoutAttributes in allLayoutAttributes! {
+            let indexPath = layoutAttributes.indexPath
+            indexPaths.append(indexPath)
+        }
+        return indexPaths
+    }
+}
+
+internal extension NSIndexSet {
+    
+    func aapl_indexPathsFromIndexesWithSection(section: Int) -> [NSIndexPath] {
+        var indexPaths: [NSIndexPath] = []
+        indexPaths.reserveCapacity(self.count)
+        self.enumerateIndexesUsingBlock {idx, stop in
+            indexPaths.append(NSIndexPath(forItem: idx, inSection: section))
+        }
+        return indexPaths
+    }
+}
+
+private extension FSAlbumView {
+    
+    func changeImage(asset: PHAsset) {
+        
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            
+            let options = PHImageRequestOptions()
+            options.networkAccessAllowed = true
+            
+            self.imageManager?.requestImageForAsset(asset,
+                targetSize: CGSize(width: asset.pixelWidth, height: asset.pixelHeight),
+                contentMode: .AspectFill,
+                options: options) {
+                    result, info in
+                    
+            }
+        })
+    }
+    
+    // Check the status of authorization for PHPhotoLibrary
+    private func checkPhotoAuth() {
+        
+        PHPhotoLibrary.requestAuthorization { (status) -> Void in
+            switch status {
+            case .Authorized:
+                self.imageManager = PHCachingImageManager()
+                if self.images != nil && self.images.count > 0 {
+                    
+                    self.changeImage(self.images[0] as! PHAsset)
+                }
+                
+            case .Restricted, .Denied:
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    self.delegate?.albumViewCameraRollUnauthorized()
+                    
+                })
+            default:
+                break
+            }
+        }
+    }
+
+    // MARK: - Asset Caching
+    
+    func resetCachedAssets() {
+        
+        imageManager?.stopCachingImagesForAllAssets()
+        previousPreheatRect = CGRectZero
+    }
+ 
+    func updateCachedAssets() {
+        
+        var preheatRect = self.collectionView!.bounds
+        preheatRect = CGRectInset(preheatRect, 0.0, -0.5 * CGRectGetHeight(preheatRect))
+        
+        let delta = abs(CGRectGetMidY(preheatRect) - CGRectGetMidY(self.previousPreheatRect))
+        if delta > CGRectGetHeight(self.collectionView!.bounds) / 3.0 {
+            
+            var addedIndexPaths: [NSIndexPath] = []
+            var removedIndexPaths: [NSIndexPath] = []
+            
+            self.computeDifferenceBetweenRect(self.previousPreheatRect, andRect: preheatRect, removedHandler: {removedRect in
+                let indexPaths = self.collectionView.aapl_indexPathsForElementsInRect(removedRect)
+                removedIndexPaths += indexPaths
+                }, addedHandler: {addedRect in
+                    let indexPaths = self.collectionView.aapl_indexPathsForElementsInRect(addedRect)
+                    addedIndexPaths += indexPaths
+            })
+            
+            let assetsToStartCaching = self.assetsAtIndexPaths(addedIndexPaths)
+            let assetsToStopCaching = self.assetsAtIndexPaths(removedIndexPaths)
+            
+            self.imageManager?.startCachingImagesForAssets(assetsToStartCaching,
+                targetSize: cellSize,
+                contentMode: .AspectFill,
+                options: nil)
+            self.imageManager?.stopCachingImagesForAssets(assetsToStopCaching,
+                targetSize: cellSize,
+                contentMode: .AspectFill,
+                options: nil)
+            
+            self.previousPreheatRect = preheatRect
+        }
+    }
+    
+    func computeDifferenceBetweenRect(oldRect: CGRect, andRect newRect: CGRect, removedHandler: CGRect->Void, addedHandler: CGRect->Void) {
+        if CGRectIntersectsRect(newRect, oldRect) {
+            let oldMaxY = CGRectGetMaxY(oldRect)
+            let oldMinY = CGRectGetMinY(oldRect)
+            let newMaxY = CGRectGetMaxY(newRect)
+            let newMinY = CGRectGetMinY(newRect)
+            if newMaxY > oldMaxY {
+                let rectToAdd = CGRectMake(newRect.origin.x, oldMaxY, newRect.size.width, (newMaxY - oldMaxY))
+                addedHandler(rectToAdd)
+            }
+            if oldMinY > newMinY {
+                let rectToAdd = CGRectMake(newRect.origin.x, newMinY, newRect.size.width, (oldMinY - newMinY))
+                addedHandler(rectToAdd)
+            }
+            if newMaxY < oldMaxY {
+                let rectToRemove = CGRectMake(newRect.origin.x, newMaxY, newRect.size.width, (oldMaxY - newMaxY))
+                removedHandler(rectToRemove)
+            }
+            if oldMinY < newMinY {
+                let rectToRemove = CGRectMake(newRect.origin.x, oldMinY, newRect.size.width, (newMinY - oldMinY))
+                removedHandler(rectToRemove)
+            }
+        } else {
+            addedHandler(newRect)
+            removedHandler(oldRect)
+        }
+    }
+    
+    func assetsAtIndexPaths(indexPaths: [NSIndexPath]) -> [PHAsset] {
+        if indexPaths.count == 0 { return [] }
+        
+        var assets: [PHAsset] = []
+        assets.reserveCapacity(indexPaths.count)
+        for indexPath in indexPaths {
+            let asset = self.images[indexPath.item] as! PHAsset
+            assets.append(asset)
+        }
+        return assets
+    }
+}
---
+++ pummel/Sources/FusumaViewController.swift
@@ -0,0 +1,257 @@
+//
+//  FusumaViewController.swift
+//  Fusuma
+//
+//  Created by Thong Nguyen on 2015/11/14.
+//  Copyright © 2015年 Thong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+@objc public protocol FusumaDelegate: class {
+    
+    func fusumaImageSelected(image: UIImage)
+    func fusumaCameraRollUnauthorized()
+    
+    optional func fusumaClosed()
+    optional func fusumaDismissedWithImage(image: UIImage)
+}
+
+public var fusumaBaseTintColor       = UIColor.hex("#000000", alpha: 1.0)
+public var fusumaBaseWhiteTintColor       = UIColor.hex("#FFFFFF", alpha: 1.0)
+public var fusumaTintColor       = UIColor.hex("#FFFFFF", alpha: 1.0)
+public var fusumaBackgroundColor = UIColor.hex("#000000", alpha: 1.0)
+public var fusumaBackgroundBlackColor = UIColor.hex("#000000", alpha: 1.0)
+
+public func changeMode() {}
+
+public enum FusumaMode {
+    case Camera
+    case Library
+}
+
+public enum FusumaModeOrder {
+    case CameraFirst
+    case LibraryFirst
+}
+
+@objc public class FusumaViewController: UIViewController, FSCameraViewDelegate, FSAlbumViewDelegate {
+
+    private var mode: FusumaMode?
+    public var defaultMode: FusumaMode?
+    public var modeOrder: FusumaModeOrder = .LibraryFirst
+    public var willFilter = true
+
+    @IBOutlet weak var photoLibraryViewerContainer: UIView!
+    @IBOutlet weak var cameraShotContainer: UIView!
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var libraryButton: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+
+    @IBOutlet var libraryFirstConstraints: [NSLayoutConstraint]!
+    @IBOutlet var cameraFirstConstraints: [NSLayoutConstraint]!
+    @IBOutlet var menuHeighConstraint: NSLayoutConstraint!
+    
+    var albumView  = FSAlbumView.instance()
+    var cameraView = FSCameraView.instance()
+    
+    public weak var delegate: FusumaDelegate? = nil
+    
+    override public func loadView() {
+        
+        if let view = UINib(nibName: "FusumaViewController", bundle: NSBundle(forClass: self.classForCoder)).instantiateWithOwner(self, options: nil).first as? UIView {
+            
+            self.view = view
+        }
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        cameraView.delegate = self
+        albumView.delegate  = self
+        titleLabel.font = UIFont(name: "Montserrat-Regular", size: 13)
+        doneButton.titleLabel?.font = UIFont(name: "Montserrat-Light", size: 13)!
+
+        menuView.backgroundColor = fusumaBackgroundColor
+        menuView.addBottomBorder(UIColor.blackColor(), width: 1.0)
+        
+		libraryButton.tintColor = fusumaTintColor
+		
+        cameraButton.adjustsImageWhenHighlighted  = false
+        libraryButton.adjustsImageWhenHighlighted = false
+        cameraButton.clipsToBounds  = true
+        libraryButton.clipsToBounds = true
+
+        changeMode(defaultMode ?? FusumaMode.Library)
+        photoLibraryViewerContainer.addSubview(albumView)
+        cameraShotContainer.addSubview(cameraView)
+        
+        doneButton.tintColor = fusumaBaseTintColor
+
+		titleLabel.textColor = fusumaBaseTintColor
+		
+        if modeOrder != .LibraryFirst {
+            libraryFirstConstraints.forEach { $0.priority = 250 }
+            cameraFirstConstraints.forEach { $0.priority = 1000 }
+        }
+        
+        if modeOrder == .CameraFirst {
+            self.menuHeighConstraint.constant = 44
+            self.menuView.backgroundColor = UIColor.blackColor()
+        } else {
+            self.menuHeighConstraint.constant = 70
+            self.menuView.backgroundColor = UIColor.whiteColor()
+        }
+    }
+    
+    override public func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+         self.view.backgroundColor = fusumaBackgroundColor
+        
+    }
+
+    override public func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        albumView.frame  = CGRect(origin: CGPointZero, size: photoLibraryViewerContainer.frame.size)
+        albumView.layoutIfNeeded()
+        cameraView.frame = CGRect(origin: CGPointZero, size: cameraShotContainer.frame.size)
+        cameraView.layoutIfNeeded()
+        
+        albumView.initialize()
+        cameraView.initialize()
+        cameraView.goLibrary.addTarget(self, action:"libraryButtonPressed:", forControlEvents:UIControlEvents.TouchUpInside)
+    }
+
+    override public func prefersStatusBarHidden() -> Bool {
+        if modeOrder == .CameraFirst {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    @IBAction func closeButtonPressed(sender: UIButton) {
+
+        self.dismissViewControllerAnimated(true, completion: {
+            
+            self.delegate?.fusumaClosed?()
+        })
+    }
+    
+    @IBAction func libraryButtonPressed(sender: UIButton) {
+        
+        changeMode(FusumaMode.Library)
+    }
+    
+    @IBAction func photoButtonPressed(sender: UIButton) {
+    
+        changeMode(FusumaMode.Camera)
+    }
+    
+    @IBAction func doneButtonPressed(sender: UIButton) {
+        
+        let image = albumView.imageSelected as UIImage
+        delegate?.fusumaImageSelected(image)
+        self.dismissViewControllerAnimated(true, completion: {
+            
+            self.delegate?.fusumaDismissedWithImage?(image)
+        })
+    }
+    
+    // MARK: FSCameraViewDelegate
+    func cameraShotFinished(image: UIImage) {
+        
+        delegate?.fusumaImageSelected(image)
+        self.dismissViewControllerAnimated(true, completion: {
+        
+            self.delegate?.fusumaDismissedWithImage?(image)
+        })
+    }
+    
+    // MARK: FSAlbumViewDelegate
+    public func albumViewCameraRollUnauthorized() {
+        
+        delegate?.fusumaCameraRollUnauthorized()
+    }
+}
+
+private extension FusumaViewController {
+    
+    func changeMode(mode: FusumaMode) {
+
+        if self.mode == mode {
+            
+            return
+        }
+        
+        self.mode = mode
+        
+        dishighlightButtons()
+        
+        if mode == FusumaMode.Library {
+            titleLabel.text = NSLocalizedString("CAMERA ROLL", comment: "CAMERA ROLL")
+            titleLabel.font = UIFont(name: "Montserrat-Regular", size: 13)
+            doneButton.hidden = false
+            doneButton.titleLabel?.font = UIFont(name: "Montserrat-Light", size: 13)!
+            self.menuHeighConstraint.constant = 70
+            self.menuView.backgroundColor = UIColor.whiteColor()
+            titleLabel.hidden = false
+            closeButton.hidden = true
+            highlightButton(libraryButton)
+            self.view.insertSubview(photoLibraryViewerContainer, aboveSubview: cameraShotContainer)
+            
+        } else {
+            doneButton.hidden = true
+            titleLabel.hidden = true
+            self.menuView.backgroundColor = UIColor.blackColor()
+            highlightButton(cameraButton)
+            self.view.insertSubview(cameraShotContainer, aboveSubview: photoLibraryViewerContainer)
+        }
+    }
+    
+    
+    func dishighlightButtons() {
+        
+        cameraButton.tintColor  = fusumaBaseTintColor
+        libraryButton.tintColor = fusumaBaseTintColor
+        
+        if cameraButton.layer.sublayers?.count > 1 {
+            
+            for layer in cameraButton.layer.sublayers! {
+                
+                if let borderColor = layer.borderColor where UIColor(CGColor: borderColor) == fusumaTintColor {
+                    
+                    layer.removeFromSuperlayer()
+                }
+                
+            }
+        }
+        
+        if libraryButton.layer.sublayers?.count > 1 {
+            
+            for layer in libraryButton.layer.sublayers! {
+                
+                if let borderColor = layer.borderColor where UIColor(CGColor: borderColor) == fusumaTintColor {
+                    
+                    layer.removeFromSuperlayer()
+                }
+                
+            }
+        }
+        
+    }
+    
+    func highlightButton(button: UIButton) {
+        
+        button.tintColor = fusumaTintColor
+        
+        button.addBottomBorder(fusumaTintColor, width: 3)
+    }
+}
