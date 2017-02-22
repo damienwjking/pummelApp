c588fdff14b57cd1818da4248834ce3d6361ea15
Henry <Henry>
2017-02-23 00:21:30 +0700

    change animation of select result search coach

---
+++ pummel/CardViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CardViewCell.swift
+//  pummel
+//
+//  Created by Nguyễn Tấn Phúc on 2/22/17.
+//  Copyright © 2017 pummel. All rights reserved.
+//
+
+import UIKit
+
+class CardViewCell: UICollectionViewCell {
+    @IBOutlet weak var cardView: CardView!
+    
+}
