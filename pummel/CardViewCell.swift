7ed38e22593954ef04b238f16437e4d1e5d89331
Henry <Henry>
2017-04-24 21:04:36 +0700

    card view add more info button

--- pummel/CardViewCell.swift
+++ pummel/CardViewCell.swift
@@ -15,10 +15,16 @@ import UIKit
 class CardViewCell: UICollectionViewCell, CardViewDelegate {
     @IBOutlet weak var cardView: CardView!
     
+    @IBOutlet weak var moreInfoLabel: UILabel!
+    @IBOutlet weak var moreInfoLeftView: UIView!
+    
     weak var delegate : CardViewCellDelegate? = nil
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        self.moreInfoLabel.font = UIFont.pmmMonReg13()
+        self.moreInfoLeftView.layer.cornerRadius = self.moreInfoLeftView.frame.size.height/2;
         
         self.cardView.delegate = self
         self.cardView.registerTagCell()
