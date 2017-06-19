c2c901fb8dc419cd0bc2811306687ec1b167633c
Henry <phuc@adaptivemedia.com.au>
2017-06-19 16:59:33 +0700

    update video upload

---
+++ pummel/CameraViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CameraViewController.swift
+//  pummel
+//
+//  Created by Nguyễn Tấn Phúc on 6/19/17.
+//  Copyright © 2017 pummel. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+    }
+
+}
