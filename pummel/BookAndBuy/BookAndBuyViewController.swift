dcde57169fcc6722e20d5bcc12e6806871b20ef7
Henry <phuc@adaptivemedia.com.au>
2017-10-25 09:40:09 +0700

    Init product

---
+++ pummel/BookAndBuy/BookAndBuyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BookAndBuyViewController.swift
+//  pummel
+//
+//  Created by Nguyễn Tấn Phúc on 10/24/17.
+//  Copyright © 2017 pummel. All rights reserved.
+//
+
+import UIKit
+
+class BookAndBuyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ pummel/BookAndBuy/Cell/BookAndBuyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookAndBuyTableViewCell.swift
+//  pummel
+//
+//  Created by Nguyễn Tấn Phúc on 10/24/17.
+//  Copyright © 2017 pummel. All rights reserved.
+//
+
+import UIKit
+
+class BookAndBuyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
