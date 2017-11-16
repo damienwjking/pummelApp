ebe684caab8da2e740fbf8dbf97f4d883a070e59
Henry <phuc@adaptivemedia.com.au>
2017-11-16 14:46:00 +0700

    refacter code

--- pummel/Extension.swift
+++ pummel/Extension.swift
@@ -148,7 +148,9 @@ extension String {
     }
     
     func isValidEmail() -> Bool {
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", kEmailRegEx)
+        let emailRegEx = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: self)
     }
     
